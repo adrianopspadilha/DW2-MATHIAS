49d2f83eeef0420c171b1f7e73eb000e9933b71c
adrianopspadilha <adrianopspadilha@gmail.com>
2023-09-23 23:15:49 -0300

    correcao

--- aula03/script3.js
+++ aula03/script3.js
@@ -1,23 +1,23 @@
-function calcDesconto(valorCompra, possuiConvenio, possuiCartaoLoja){
-        if (possuiConvenio && pagamentoCartaoLoja) {
-        valorDesconto = valorCompra * 0.15
-        if (valorDesconto > 100) {
-        valorDesconto = 100;
+function calcDesconto(valorCompra, possuiConvenio, possuiCartaoLoja) {
+        if (possuiConvenio && possuiCartaoLoja) {
+                valorDesconto = valorCompra * 0.15
+                if (valorDesconto > 100) {
+                        valorDesconto = 100;
+                }
+                valorCompra = valorCompra - valorDesconto;
+                return valorCompra
         }
-        valorCompra = valorCompra - valorDesconto;
-        return valorCompra
-        }
-        if (possuiConvenio || pagamentoCartaoLoja) {
-        valorDesconto = valorCompra * 0.10;
-        if (valorDesconto > 100) {
-        valorDesconto = 100;
-        }
-        valorCompra = valorCompra - valorDesconto;
-        return valorCompra
-        
+        if (possuiConvenio || possuiCartaoLoja) {
+                valorDesconto = valorCompra * 0.10;
+                if (valorDesconto > 100) {
+                        valorDesconto = 100;
+                }
+                valorCompra = valorCompra - valorDesconto;
+                return valorCompra
+
         }
         return valorCompra;
-        }
-        
-        const primeiraCompra = convenioFarmacia(2000, true, true);
-        console.log(primeiraCompra);
+}
+
+const primeiraCompra = calcDesconto(1000, true, true);
+console.log(primeiraCompra);
